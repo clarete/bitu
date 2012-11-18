59cc6b6380a760bda3f06e222c9476e983cf8103
Lincoln de Sousa <lincoln@comum.org>
2012-11-18 17:25:33 -0500

    Small improvements in the irc client

--- src/transport-irc.c
+++ src/transport-irc.c
@@ -19,6 +19,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 #include <libircclient/libircclient.h>
 #include <bitu/errors.h>
 #include <bitu/transport.h>
@@ -32,20 +33,55 @@ void _irc_event_notice (irc_session_t *session,
                         const char **params,
                         unsigned int count)
 {
-  char buf[512];
-  unsigned int cnt;
+  regex_t re;
+  size_t i;
+  char origin_nick[128];
+  char pattern[128];
+  const char *cmd;
 
-  buf[0] = '\0';
+  bitu_command_t *command = NULL;
+  bitu_transport_t *transport = (bitu_transport_t *) irc_get_ctx (session);
+  const char *nick = ta_iri_get_user (bitu_transport_get_uri (transport));
+  const char *channel = ta_iri_get_fragment (bitu_transport_get_uri (transport));
 
-  for (cnt = 0; cnt < count; cnt++)
-    {
-      if (cnt)
-        strcat (buf, "|");
-      strcat (buf, params[cnt]);
-    }
+  /* Is this message to me? */
+  snprintf (pattern, sizeof (pattern), "[[:<:]]%s[[:>:]]", nick);
+  if (regcomp (&re, pattern, REG_EXTENDED) != 0)
+    return;
+  if (regexec (&re, &params[1][0], 0, NULL, REG_ENHANCED | REG_NOSUB) != 0)
+    return;
 
-  fprintf (stderr, "Event: %s, origin: %s, params: %d [%s]\n", event, origin ? origin : NULL, cnt, buf);
+  /* Getting the origin nickname */
+  if (origin)
+    irc_target_get_nick (origin, origin_nick, sizeof (origin_nick));
+  else
+    strncpy (origin_nick, channel, sizeof (origin_nick));
+
+  /* Sending the command to the queue */
+  cmd = params[1];
+  for (i = 0; i < strlen (nick) + 1; i++, *cmd++);
+  command = bitu_command_new (transport, cmd, origin_nick);
+  bitu_transport_queue_command (transport, command);
+
+  /* XXX: Debug trash */
+  {
+    unsigned int cnt;
+    char buf[512];
+    buf[0] = '\0';
+
+    for (cnt = 0; cnt < count; cnt++)
+      {
+        if (cnt)
+          strcat (buf, "|");
+        strcat (buf, params[cnt]);
+      }
+
+    fprintf (stderr,
+             "Event: %s, origin: %s, params: %d [%s]\n",
+             event, origin_nick, cnt, buf);
+  }
 }
+
 
 void
 _irc_event_connect (irc_session_t *session,
@@ -54,10 +90,12 @@ _irc_event_connect (irc_session_t *session,
                     const char **params,
                     unsigned int count)
 {
-  char *channel = (char *) irc_get_ctx (session);
+  bitu_transport_t *transport = (bitu_transport_t *) irc_get_ctx (session);
+  const char *channel = ta_iri_get_fragment (bitu_transport_get_uri (transport));
   printf ("Channel: %s\n", channel);
   irc_cmd_join (session, channel, 0);
 }
+
 
 static irc_session_t *
 _irc_get_session (bitu_transport_t *transport)
@@ -68,6 +106,7 @@ _irc_get_session (bitu_transport_t *transport)
   memset (&callbacks, 0, sizeof(callbacks));
   callbacks.event_connect = _irc_event_connect;
   callbacks.event_notice = _irc_event_notice;
+  callbacks.event_channel = _irc_event_notice;
   callbacks.event_channel_notice = _irc_event_notice;
   callbacks.event_privmsg = _irc_event_notice;
 
@@ -78,7 +117,7 @@ _irc_get_session (bitu_transport_t *transport)
       return NULL;
     }
 
-  irc_set_ctx (session, (void *) ta_iri_get_fragment (bitu_transport_get_uri (transport)));
+  irc_set_ctx (session, (void *) transport);
   return session;
 }
 
@@ -95,36 +134,37 @@ _irc_connect (bitu_transport_t *transport)
   port = ta_iri_get_port (bitu_transport_get_uri (transport));
   port = port ? port : IRC_PORT;
 
-  printf ("Connecting...");
   if (irc_connect (bitu_transport_get_data (transport), host, port, NULL, nick, 0, 0) != 0)
     {
-      printf ("bosta: %s\n",
-              irc_strerror (irc_errno (bitu_transport_get_data (transport))));
       ta_error_set (BITU_ERROR_TRANSPORT_IRC_CONN,
                     irc_strerror (irc_errno (bitu_transport_get_data (transport))));
-      printf ("error\n");
       return TA_ERROR;
     }
-  printf ("ok\n");
   return TA_OK;
 }
+
 
 static int
 _irc_run (bitu_transport_t *transport)
 {
-  printf ("Running...\n");
   if (irc_run (bitu_transport_get_data (transport)) != 0)
     {
-      printf ("bosta: %s\n",
-              irc_strerror (irc_errno (bitu_transport_get_data (transport))));
       ta_error_set (BITU_ERROR_TRANSPORT_IRC_RUN,
                     irc_strerror (irc_errno (bitu_transport_get_data (transport))));
-      printf ("error\n");
       return TA_ERROR;
     }
-  printf ("ok\n");
   return TA_OK;
 }
+
+
+static int
+_irc_send (bitu_transport_t *transport, const char *msg, const char *to)
+{
+  irc_session_t *session = bitu_transport_get_data (transport);
+  printf ("msg: %s: %s\n", to, msg);
+  return irc_cmd_msg (session, to, msg) == 0 ? TA_OK : TA_ERROR;
+}
+
 
 int
 _bitu_irc_transport (bitu_transport_t *transport)
@@ -132,6 +172,7 @@ _bitu_irc_transport (bitu_transport_t *transport)
   irc_session_t *session;
   bitu_transport_set_callback_connect (transport, _irc_connect);
   bitu_transport_set_callback_run (transport, _irc_run);
+  bitu_transport_set_callback_send (transport, _irc_send);
 
   if ((session = _irc_get_session (transport)) == NULL)
       return TA_ERROR;
